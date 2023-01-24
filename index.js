8142e3156c2ed5b1f0a518ab13d3289b9d161985
MelissaRV <melissa.rvillegas206@gmail.com>
2023-01-24 12:33:25 -0600

    test / readingFiles

--- index.js
+++ index.js
@@ -11,29 +11,43 @@ const isMarkdown = (filePath) => path.extname(filePath) === '.md';
 const getAbsolutePath = (filePath) =>
 (path.isAbsolute(filePath)) ? filePath : path.resolve(filePath);
 
+// leer la ruta y pasarla a utf8
+// const route = './node.txt'
+const readingFiles = (route) => new Promise((resolve, reject) => {
+  fs.readFile(route, 'utf8', function(error, data) {
+    if (error) {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject('error');
+    } else {
+      resolve(data);
+    }
+  });
+});
 /* ---------Función MdLinks------ */
 const mdLinks = ( path, options) => {
-  const absolute = getAbsolutePath(path);
   return new Promise((resolve, reject) => {
     // identificar si la ruta existe
-    if (!doesPathExist(absolute)) {
+    if (doesPathExist(path)) {
       // eslint-disable-next-line prefer-promise-reject-errors
-      return reject(`${path}the path DOESNT exists`);
+      const absolute = getAbsolutePath(path);
+      if (isFile(absolute)) {
       // Revisar o convertir a ruta absoluta
-    } else {
+        console.log('the path exist');
+      } else {
       // si no existe la ruta la promesa se rechaz
       // eslint-disable-next-line prefer-promise-reject-errors
-      resolve('the path exist');
+        reject('the path DOSENT exist');
+      }
+      readingFiles('./node.txt').then((res) => (res));
     };
   });
 };
-
+console.log(mdLinks('./node.txt'));
 module.exports = {
   mdLinks,
   isMarkdown,
   doesPathExist,
   getAbsolutePath,
   isFile,
+  readingFiles,
 };
-
-
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,6 @@
-const {mdLinks} = require('../index.js');
+const {mdLinks, isFile,
+  readingFiles,
+} = require('../index.js');
 
 
 describe('mdLinks', () => {
@@ -6,11 +8,24 @@ describe('mdLinks', () => {
     console.log('FIX ME!');
   });
   /* it('should return a promise', () => {
-    expect(mdLinks()).toBe(typeof Promise);
-  });*/
+    expect(mdLinks('./README.md')).toBe(typeof Promise);
+  }); */
   it('should reject non-existent path', () => {
-    return mdLinks('/melissa/dev001/carpeta.md').catch((error) => {
-      expect(error).toBe('the path DOESN`T exists');
+    mdLinks('./melissa/dev001/carpeta.md').catch((error) => {
+      expect(error).toBe('the path DOESNT exists');
     });
+  });
+});
+describe('isFile', () => {
+  it('should be a function', () => {
+    expect(typeof isFile).toBe('function');
+  });
+  it('isFile should return true if its a file', () => {
+    expect(isFile('./node.txt')).toBe(true);
+  });
+});
+describe('readingFile', () => {
+  it('shoul be a function', () => {
+    expect(typeof readingFiles).toBe('function');
   });
 });
